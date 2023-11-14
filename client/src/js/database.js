39c343844d6c268ed3852f92f5c3eda8c46c216a
jtuvek <132848981+jtuvek@users.noreply.github.com>
2023-11-13 22:43:13 -0500

    completed database and install js files

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,29 @@ const initdb = async () =>
     },
   });
 
+const dbPromise = initdb(); // Make sure to store the promise for later use
+
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const db = await dbPromise;
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const id = await store.put({ content }); // Assuming the structure of your object is { content: 'yourContent' }
+  await tx.complete;
+  console.log(`Content added to the database with id: ${id}`);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await dbPromise;
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const content = await store.getAll();
+  await tx.complete;
+  console.log('All content retrieved from the database:', content);
+  return content;
+};
 
+// Initialize the database
 initdb();
+
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,46 @@
 const butInstall = document.getElementById('buttonInstall');
 
+let deferredPrompt; // Store the event for later use
+
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+  // Prevent the default behavior of the event
+  event.preventDefault();
+
+  // Store the event for later use
+  deferredPrompt = event;
+
+  // Update the UI to notify the user that the app can be installed
+  butInstall.style.display = 'block';
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+  if (deferredPrompt) {
+    // Show the install prompt
+    deferredPrompt.prompt();
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+    // Wait for the user to respond to the prompt
+    const choiceResult = await deferredPrompt.userChoice;
+
+    // Check if the user accepted the prompt
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the install prompt');
+    } else {
+      console.log('User dismissed the install prompt');
+    }
+
+    // Reset the deferredPrompt variable
+    deferredPrompt = null;
+
+    // Hide the install button
+    butInstall.style.display = 'none';
+  }
+});
+
+// TODO: Add a handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+  console.log('App installed successfully');
+  // Perform actions after the app is installed, if needed
+});
